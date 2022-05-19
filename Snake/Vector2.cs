64ddb0d0cc8dd570f8637d742236b8b7036bc974
NeonCrafter13 <62461337+NeonCrafter13@users.noreply.github.com>
2022-05-19 07:52:56 +0200

    Add files via upload

---
+++ Snake/Vector2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Vector2d
+    {
+        public int posx { get; set; }
+        public int posy { get; set; }
+
+        public bool IsTheSame(Vector2d _o)
+        {
+            if (posx == _o.posx && posy == _o.posy)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void update_x(int amount)
+        {
+            posx += amount;
+        }
+
+        public void update_y(int amount)
+        {
+            posy += amount;
+        }
+
+        public Vector2d(int _x, int _y)
+        {
+            posx = _x;
+            posy = _y;
+        }
+    }
+}
