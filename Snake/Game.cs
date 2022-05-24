235a2ce168517495953c87bad94c5f6d9e3d6395
Marek <marek.mueller04@gmail.com>
2022-05-24 09:16:51 +0200

    everithing working

--- Snake/Game.cs
+++ Snake/Game.cs
@@ -1,23 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace Snake
 {
     class Game
     {
         private Snake snake;
-        private Fruit fruit;
+        // private Fruit fruit;
         private int score;
         private Grid grid;
         private Menu menu;
         private bool running;
 
-        public Game(ref Menu _menu)
+        public Game(Menu _menu)
         {
             menu = _menu;
-            snake = new Snake();
-            grid = new Grid();
+            snake = new Snake(ConsoleColor.DarkRed);
+            grid = new Grid(25, 25, ConsoleColor.Green);
             score = 0;
         }
 
@@ -26,27 +27,29 @@ namespace Snake
             running = true;
             while(running)
             {
+                Thread.Sleep(500);
                 game_loop();
             }
         }
         public void reset()
         {
-            snake = new Snake();
-            grid = new Grid();
+            snake = new Snake(ConsoleColor.DarkRed);
+            grid = new Grid(25, 25, ConsoleColor.Green);
             score = 0;
         }
 
         void game_loop()
         {
             snake.move();
-
+            /*
             if (snake.segments[0].IsTheSame(fruit.postion))
             {
-                snake.addSegment();
+                snake.add_segments();
                 score++;
                 spawn_fruit();
             }
-            for (int i = 1; i<snake.segments; i++)
+            */
+            for (int i = 1; i<snake.segments.Count; i++)
             {
                 if (snake.segments[0].IsTheSame(snake.segments[i]))
                 {
@@ -54,22 +57,22 @@ namespace Snake
                     return;
                 }
             }
-            if (snake.segments[0].posx = 0)
+            if (snake.segments[0].posx == 0)
             {
                 end();
                 return;
             }
-            if (snake.segments[0].posy = 0)
+            if (snake.segments[0].posy == 0)
             {
                 end();
                 return;
             }
-            if (snake.segments[0].posx = 16)
+            if (snake.segments[0].posx == 16)
             {
                 end();
                 return;
             }
-            if (snake.segments[0].posy = 16)
+            if (snake.segments[0].posy == 16)
             {
                 end();
                 return;
@@ -79,8 +82,9 @@ namespace Snake
 
         void print()
         {
-            snake.draw();
-            fruit.draw();
+            Console.Clear();
+            snake.draw_segments();
+            // fruit.draw();
             grid.draw();
         }
 
@@ -90,10 +94,12 @@ namespace Snake
             menu.end(score);
         }
 
+        /*
         void spawn_fruit()
         {
             fruit.position = ; // Random position where there is no snake element
         }
+        */
 
     }
 }
--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -7,6 +7,13 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
+    enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
     class Snake
     {
 
@@ -14,6 +21,19 @@ namespace Snake
         public ConsoleColor snakecolor { get; set; }
 
         public string shape { get; set; }
+
+        private Direction direction;
+
+        public Snake(ConsoleColor _color)
+        {
+            snakecolor = _color;
+            segments = new List<Vector2d> {
+                new Vector2d(5, 5)
+            };
+            direction = Direction.Down;
+            shape = "#";
+        }
+
 
         public void draw(int x, int y)
         {
@@ -29,35 +49,55 @@ namespace Snake
 
             }
         }
-        public void add_segments(Vector2d v)
+        public void add_segments()
         {
-            segments.Add(v);
+            segments.Add(new Vector2d(segments.Last().posx, segments.Last().posy));
         }
 
         public void move()
         {
-            var command = Console.ReadKey().Key;
-            switch (command)
+            if (Console.KeyAvailable) {
+                var command = Console.ReadKey().Key;
+                switch (command)
+                {
+                    case ConsoleKey.DownArrow:
+                        direction = Direction.Down;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        direction = Direction.Up;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        direction = Direction.Left;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        direction = Direction.Right;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+            switch (direction)
             {
-                case ConsoleKey.DownArrow:
-                    segments[0].posy++;
-                    break;                                                  //Snake  muss noch mit Directions move,mache ich Dienstag.
-                case ConsoleKey.UpArrow:
+                case Direction.Up:
                     segments[0].posy--;
                     break;
-                case ConsoleKey.LeftArrow:
+                case Direction.Down:
+                    segments[0].posy++;
+                    break;
+                case Direction.Left:
                     segments[0].posx--;
                     break;
-                case ConsoleKey.RightArrow:
+                case Direction.Right:
                     segments[0].posx++;
                     break;
-                default:
-                    break;
             }
+
+            move_segments();
         }
         public void draw_segments()
         {
-            for (int i = 0; i <= (segments.Count - 1); i++)
+            for (int i = 0; i < segments.Count; i++)
             {
                 draw(segments[i].posx, segments[i].posy);
             }
