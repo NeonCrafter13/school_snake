bf4b2e50ba4e52e9eaea448f4928f1c144ca3972
Marek <marek.mueller04@gmail.com>
2022-05-24 09:42:43 +0200

    added ヤニクの野菜クラス

---
+++ Snake/Fruit.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Fruit
+    {
+        private int type;
+        public string shape;
+        public ConsoleColor fruitcolour;
+
+        private Vector2d position;
+
+        public Fruit(int Typ, int _x, int _y)
+        {
+            type = Typ;
+            switch (type)
+            {
+                case 0:
+                    shape = "%";
+                    fruitcolour = ConsoleColor.Blue;
+                    break;
+                case 1:
+                    shape = "#";
+                    fruitcolour = ConsoleColor.Green;
+                    break;
+                case 2:
+                    shape = "Y";
+                    fruitcolour = ConsoleColor.Yellow;
+                    break;
+                case 3:
+                    shape = "X";
+                    fruitcolour = ConsoleColor.Magenta;
+                    break;
+
+            }
+
+            position = new Vector2d(_x, _y);
+        }
+
+        public int Get_type()
+        {
+            return type;
+        }
+        public string Get_shape()
+        {
+            return shape;
+        }
+        public ConsoleColor Get_fruitcolour()
+        {
+            return fruitcolour;
+        }
+
+    }
+}
